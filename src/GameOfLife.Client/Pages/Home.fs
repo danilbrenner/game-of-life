f38a2a5aee42b6a19bf2067e2a361e8c66a95f3d
Danil <danilbrenner@outlook.com>
2023-06-19 20:22:32 +0300

    New structure

--- src/GameOfLife.Client/Pages/Home.fs
+++ src/GameOfLife.Client/Pages/Home.fs
@@ -1,5 +1,20 @@
 namespace GameOfLife.Client.Pages
 
+open Elmish
+
 module Home =
 
-    let view = ()
+    open Bolero
+    open Bolero.Html
+
+    type private InnerState = { txt: string }
+
+    type State = private State of InnerState
+
+    type Msg = Noop
+
+    let init = { txt = "Hi" } |> State
+
+    let update (State state) (msg: Msg) = State state, Cmd.none
+
+    let view (State state) = div { text "Hello!" }
